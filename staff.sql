6ef7e1af110a72c22181bd2f118097f42bb402b8
Ambrose Mwendwa <mwendwa5@gmail.com>
2024-08-26 15:08:32 +0300

    SQL

--- staff.sql
+++
@@ -1,3 +0,0 @@
-create database employees;
-create table staff(id serial primary key,staff_no int unique,fname varchar(100),lname varchar(100));
-alter table staff add constraint staffno_uniq unique (staff_no);
